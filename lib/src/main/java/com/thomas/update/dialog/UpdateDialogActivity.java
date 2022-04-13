ed4457df48fbabbf15dda57502f0aaf143738f67
ithomasoft <tom_flying@163.com>
2022-04-13 15:36:11 +0800

    base 3.0.7

--- lib/src/main/java/com/thomas/update/dialog/UpdateDialogActivity.java
+++ lib/src/main/java/com/thomas/update/dialog/UpdateDialogActivity.java
@@ -1,18 +1,16 @@
 package com.thomas.update.dialog;
 
-import android.app.Dialog;
+
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.StateListDrawable;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -20,7 +18,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import com.thomas.update.R;
@@ -32,28 +31,30 @@ import com.thomas.update.service.DownloadService;
 
 import java.io.File;
 
-public class UpdateDialog extends Dialog implements View.OnClickListener, OnDownloadListener {
+public class UpdateDialogActivity extends AppCompatActivity implements View.OnClickListener, OnDownloadListener {
 
-    private Context context;
     private DownloadManager manager;
-    private boolean forcedUpgrade;
     private Button update;
     private NumberProgressBar progressBar;
     private OnButtonClickListener buttonClickListener;
-    private int dialogImage, dialogButtonTextColor, dialogButtonColor, dialogProgressBarColor;
     private File apk;
+    private int dialogImage, dialogButtonTextColor, dialogButtonColor, dialogProgressBarColor;
+    private boolean forcedUpgrade;
     private final int install = 0x45F;
 
-    public UpdateDialog(@NonNull Context context) {
-        super(context, R.style.UpdateDialog);
-        init(context);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
+        setTitle("");
+        setContentView(R.layout.dialog_update);
+        init();
     }
 
     /**
      * 初始化布局
      */
-    private void init(Context context) {
-        this.context = context;
+    private void init() {
         manager = DownloadManager.getInstance();
         UpdateConfiguration configuration = manager.getConfiguration();
         configuration.setOnDownloadListener(this);
@@ -63,23 +64,21 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
         dialogButtonTextColor = configuration.getDialogButtonTextColor();
         dialogButtonColor = configuration.getDialogButtonColor();
         dialogProgressBarColor = configuration.getDialogProgressBarColor();
-        View view = LayoutInflater.from(context).inflate(R.layout.dialog_update, null);
-        setContentView(view);
-        setWindowSize(context);
-        initView(view);
+        setWindowSize();
+        initView();
     }
 
-    private void initView(View view) {
-        View ibClose = view.findViewById(R.id.ib_close);
-        ImageView ivBg = view.findViewById(R.id.iv_bg);
-        TextView title = view.findViewById(R.id.tv_title);
-        TextView size = view.findViewById(R.id.tv_size);
-        TextView description = view.findViewById(R.id.tv_description);
-        progressBar = view.findViewById(R.id.np_bar);
-//        progressBar.setVisibility(forcedUpgrade ? View.VISIBLE : View.GONE);
-        update = view.findViewById(R.id.btn_update);
+    private void initView() {
+        View ibClose = findViewById(R.id.ib_close);
+        ImageView ivBg = findViewById(R.id.iv_bg);
+        TextView title = findViewById(R.id.tv_title);
+        TextView size = findViewById(R.id.tv_size);
+        TextView description = findViewById(R.id.tv_description);
+        progressBar = findViewById(R.id.np_bar);
+        progressBar.setVisibility(forcedUpgrade ? View.VISIBLE : View.GONE);
+        update = findViewById(R.id.btn_update);
         update.setTag(0);
-        View line = view.findViewById(R.id.line);
+        View line = findViewById(R.id.line);
         update.setOnClickListener(this);
         ibClose.setOnClickListener(this);
         //自定义
@@ -93,10 +92,16 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
             StateListDrawable drawable = new StateListDrawable();
             GradientDrawable colorDrawable = new GradientDrawable();
             colorDrawable.setColor(dialogButtonColor);
-            colorDrawable.setCornerRadius(dip2px(context, 3));
+            colorDrawable.setCornerRadius(dip2px(this, 3));
             drawable.addState(new int[]{android.R.attr.state_pressed}, colorDrawable);
             drawable.addState(new int[]{}, colorDrawable);
-            update.setBackgroundDrawable(drawable);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                //Android系统大于等于API16，使用setBackground
+                update.setBackground(drawable);
+            } else {
+                //Android系统小于API16，使用setBackground
+                update.setBackgroundDrawable(drawable);
+            }
         }
         if (dialogProgressBarColor != -1) {
             progressBar.setReachedBarColor(dialogProgressBarColor);
@@ -106,31 +111,24 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
         if (forcedUpgrade) {
             line.setVisibility(View.GONE);
             ibClose.setVisibility(View.GONE);
-            setOnKeyListener(new OnKeyListener() {
-                @Override
-                public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-                    //屏蔽返回键
-                    return keyCode == KeyEvent.KEYCODE_BACK;
-                }
-            });
         }
         //设置界面数据
         if (!TextUtils.isEmpty(manager.getApkVersionName())) {
-            String newVersion = context.getResources().getString(R.string.dialog_new);
+            String newVersion = getResources().getString(R.string.dialog_new);
             title.setText(String.format(newVersion, manager.getApkVersionName()));
         }
         if (!TextUtils.isEmpty(manager.getApkSize())) {
-            String newVersionSize = context.getResources().getString(R.string.dialog_new_size);
+            String newVersionSize = getResources().getString(R.string.dialog_new_size);
             size.setText(String.format(newVersionSize, manager.getApkSize()));
             size.setVisibility(View.VISIBLE);
         }
         description.setText(manager.getApkDescription());
     }
 
-    private void setWindowSize(Context context) {
+    private void setWindowSize() {
         Window dialogWindow = this.getWindow();
         WindowManager.LayoutParams lp = dialogWindow.getAttributes();
-        lp.width = (int) (getWith(context) * 0.7f);
+        lp.width = (int) (getWith(this) * 0.7f);
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
         lp.gravity = Gravity.CENTER;
         dialogWindow.setAttributes(lp);
@@ -141,7 +139,7 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
         int id = v.getId();
         if (id == R.id.ib_close) {
             if (!forcedUpgrade) {
-                dismiss();
+                finish();
             }
             //回调点击事件
             if (buttonClickListener != null) {
@@ -158,13 +156,13 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
                 update.setEnabled(false);
                 update.setText(R.string.background_downloading);
             } else {
-                dismiss();
+                finish();
             }
             //回调点击事件
             if (buttonClickListener != null) {
                 buttonClickListener.onButtonClick(OnButtonClickListener.UPDATE);
             }
-            context.startService(new Intent(context, DownloadService.class));
+            startService(new Intent(this, DownloadService.class));
         }
     }
 
@@ -172,21 +170,31 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
      * 强制更新，点击进行安装
      */
     private void installApk() {
-
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setAction(Intent.ACTION_VIEW);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         Uri uri;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            uri = FileProvider.getUriForFile(context, context.getPackageName() + ".update.provider", apk);
+            uri = FileProvider.getUriForFile(this, getPackageName() + ".update.provider", apk);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         } else {
             uri = Uri.fromFile(apk);
         }
         intent.setDataAndType(uri, "application/vnd.android.package-archive");
-        context.startActivity(intent);
+        startActivity(intent);
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (forcedUpgrade) return;
+        super.onBackPressed();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(0, 0);
     }
 
     @Override
@@ -214,6 +222,11 @@ public class UpdateDialog extends Dialog implements View.OnClickListener, OnDown
             progressBar.setVisibility(View.GONE);
             update.setText(R.string.click_hint);
         }
+    }
+
+    @Override
+    public void cancel() {
+
     }
 
     @Override
